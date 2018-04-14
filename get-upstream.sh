43047026a649c71cc30f7a518ff948413740bc8d
Adam Gausmann <agausmann@fastmail.com>
2018-04-14 13:43:51 -0500

    Fix url

--- get-upstream.sh
+++ get-upstream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Because their upstream branch name is horrible...
-git pull upstream MegaMinerAI-21-Pirates-ShellAI-Dev
+git pull https://gitlab.com/siggame/Joueur.py.git MegaMinerAI-21-Pirates-ShellAI-Dev
 
