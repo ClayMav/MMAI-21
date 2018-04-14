18daa1711d5ee4fde3de45bda4c19f3721bb5f37
Adam Gausmann <agausmann@fastmail.com>
2018-04-14 17:18:25 -0500

    Add capture

--- games/pirates/ai.py
+++ games/pirates/ai.py
@@ -4,7 +4,10 @@ from joueur.base_ai import BaseAI
 
 # <<-- Creer-Merge: imports -->> 
 # you can add additional import(s) here
+CREW = 'crew'
+SHIP = 'ship'
 # <<-- /Creer-Merge: imports -->>
+
 
 class AI(BaseAI):
     def get_name(self):
@@ -15,7 +18,8 @@ class AI(BaseAI):
     def start(self):
         # <<-- Creer-Merge: start -->> 
         # replace with your start logic
-        pass
+        self.ship = None
+        self.target = None
         # <<-- /Creer-Merge: start -->>
 
     def game_updated(self):
@@ -33,9 +37,78 @@ class AI(BaseAI):
     def run_turn(self):
         # <<-- Creer-Merge: runTurn -->> 
         # Put your game logic here for runTurn
+        print(self.game.current_turn)
+
+        if not self.player._units:
+            self.player.port.spawn(CREW)
+            self.player.port.spawn(CREW)
+            self.player.port.spawn(CREW)
+        elif self.player._units[0]._ship_health == 0:
+            self.player.port.spawn(SHIP)
+        elif self.get_merchants():
+            self.capture_ship(self.player._units[0], [self.nearest_merchant(self.player._units[0])])
+        if self.player._units:
+            print("   {}".format(self.player._units[0]._crew))
 
         return True
         # <<-- /Creer-Merge: runTurn -->>
+
+    def sq_distance(self, a, b):
+        return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
+
+
+    def nearest_merchant(self, unit):
+        ms = self.get_merchants()
+        if not ms:
+            return None
+
+        x = self.sq_distance(unit.tile, ms[0].tile)
+        result = ms[0]
+        for m in ms[1:]:
+            d = self.sq_distance(unit.tile, m.tile)
+            if d < x:
+                x = d
+                result = m
+
+        return result
+        
+
+    def get_merchants(self):
+        return [u for u in self.game.units if u.owner is None]
+
+    def attack_ship(self, unit, targets):
+        pass
+
+    def capture_ship(self, unit, targets):
+        """
+        Makes progress toward capturing a target ship.
+
+        :param unit: The friendly unit.
+        :param target: The list of units to try to capture.
+
+        :returns: True if the action has been completed, False if still in progress.
+        :rtype: bool
+        """
+
+        path = self.find_path(unit.tile, [t.tile for t in targets][0], unit)
+        while path and not any(unit.tile.has_neighbor(t.tile) for t in targets):
+            if not unit.move(path.pop(0)):
+                return False
+
+        for target in targets:
+            if unit.tile.has_neighbor(target.tile):
+                if target.crew > 0:
+                    unit.attack(target.tile, CREW)
+                    break
+                else:
+                    b = unit.split(target.tile, 1)
+                    print(b)
+                    return b
+
+        return False
+
+    def heal(self, unit):
+        pass
 
     def find_path(self, start, goal, unit):
 
