5621f95c28ba4ef178995c7bf446d6dd3b4a1385
Clay McGinnis <mcginnis@claymav.com>
2018-04-14 20:53:08 -0500

    More colors

--- games/pirates/ai.py
+++ games/pirates/ai.py
@@ -5,7 +5,6 @@ from joueur.base_ai import BaseAI
 # <<-- Creer-Merge: imports -->> 
 # you can add additional import(s) here
 from collections import OrderedDict
-from .utils import pathing
 from termcolor import colored
 import heapq
 
@@ -20,48 +19,35 @@ CREW = 'crew'
 class AI(BaseAI):
     """ The basic AI functions that are the same between games. """
 
-    def register_ship(self, crew_required):
-        port_unit = self.player.port.tile.unit
-        if port_unit and port_unit.ship_health > 0:
-            # if ship on port
-            crew_needed = crew_required - port_unit.crew
-            if crew_needed > 0:
-                self.ship_queue[port_unit] = crew_needed
-                # returns the ship unit if built all crew needed, False if not
-                return self.build()
-        else:
-            # create ship
-            self.player.port.spawn(SHIP)
-
     def get_name(self):
-        # <<-- Creer-Merge: get-name -->> 
+        # <<-- Creer-Merge: get-name -->>
         return "btw_i_use_arch"
         # <<-- /Creer-Merge: get-name -->>
 
     def start(self):
-        # <<-- Creer-Merge: start -->> 
+        # <<-- Creer-Merge: start -->>
         # replace with your start logic
         self.ship = None
         self.target = None
-        self.attackers = []
         # <<-- /Creer-Merge: start -->>
 
     def game_updated(self):
-        # <<-- Creer-Merge: game-updated -->> 
+        # <<-- Creer-Merge: game-updated -->>
         # replace with your game updated logic
         pass
         # <<-- /Creer-Merge: game-updated -->>
 
     def end(self, won, reason):
-        # <<-- Creer-Merge: end -->> 
+        # <<-- Creer-Merge: end -->>
         # replace with your end logic
         pass
         # <<-- /Creer-Merge: end -->>
 
     def run_turn(self):
-        # <<-- Creer-Merge: runTurn -->> 
+        # <<-- Creer-Merge: runTurn -->>
         # Put your game logic here for runTurn
-        print(colored('[+]', 'red'), "Turn #{}".format(self.game.current_turn))
+        print(colored('[+]', 'blue'), "Turn {}".format(self.game.current_turn))
+        attackers = []
 
         if not self.player.units:
             self.player.port.spawn(CREW)
@@ -73,20 +59,29 @@ class AI(BaseAI):
             for _ in range(self.player.gold//200):
                 self.player.port.spawn(CREW)
 
+        # Add attackers
         if len(self.player.units) > 2:
             for pawn in self.player.units[2:]:
-                self.attackers.append(pawn)
-            
+                attackers.append(pawn)
+            print(colored('[+]', 'grey'), "There are {} attackers".format(
+                len(attackers)))
+
         if self.get_neutrals():
             self.capture_ship([self.player.units[0]], self.get_neutrals())
 
         for pawn in self.player.units:
-            if pawn.ship_health != 0 and pawn.ship_health < 8 and pawn.tile is not None:
+            if pawn.ship_health != 0 and pawn.ship_health < 10 and pawn.tile is not None:
                 self.heal(pawn)
 
-        for fighter in self.attackers:
+        for pawn in self.player.units:
+            if pawn.gold >= 600 and pawn.tile is not None:
+                self.drop_off(pawn)
+
+        for fighter in attackers:
             if fighter.tile is not None:
-                self.attack_ship([fighter], self.player.opponent.units)
+                enemy_units = [u for u in self.player.opponent.units if u.tile
+                               is not None]
+                self.attack_ship([fighter], enemy_units)
 
         return True
         # <<-- /Creer-Merge: runTurn -->>
@@ -154,8 +149,29 @@ class AI(BaseAI):
     def heal(self, unit):
         """
         Moves a target to the port and heals it back to full health.
+        Also deposits currency.
 
         :param unit: The unit to heal.
+        
+        :returns: True if the action has been completed, False if still in progress.
+        :rtype: bool
+        """
+        if self.move_to_port(unit):
+            unit.rest()
+            return unit.ship_health == 20 and unit.crew_health == 4 * unit.crew
+        return False
+
+    def drop_off(self, unit):
+        if self.move_to_port(unit):
+            unit.deposit()
+            return True
+        return False
+
+    def move_to_port(self, unit):
+        """
+        Moves a target to the port.
+
+        :param unit: The unit to move.
         
         :returns: True if the action has been completed, False if still in progress.
         :rtype: bool
@@ -164,9 +180,10 @@ class AI(BaseAI):
             return False
 
         if unit.tile.has_neighbor(self.player.port.tile):
-            unit.rest()
-            return unit.ship_health == 20 and unit.crew_health == 4 * unit.crew
+            return True
+
         return False
+
 
     def move(self, src, dst):
         """
@@ -191,14 +208,13 @@ class AI(BaseAI):
         """
         Collects all of neighbors surrounding a tile with no restrictions.
         """
-        return [x for x in tile.get_neighbors() 
-            if x.type == "water" and (x.port == None or self.player.port == x.port)]
+        return [x for x in tile.get_neighbors()
+                if x.type == "water" and (x.port is None or self.player.port ==
+                                          x.port)]
 
-        # return tile.get_neighbors()
-
-    def find_path(self, start_tiles, goal_tiles, 
-            get_neighbors=None,
-            g_func=lambda x, y: 1, f_func=lambda x, y: 0):
+    def find_path(self, start_tiles, goal_tiles,
+                  get_neighbors=None,
+                  g_func=lambda x, y: 1, f_func=lambda x, y: 0):
         """
         Given a list of starting locations and a list of ending locations,
         find the shortest path between them. Uses a priority queue to
@@ -245,5 +261,18 @@ class AI(BaseAI):
                     path_from[neighbor] = working_tile
                     heapq.heappush(frontier, (f_score[neighbor], neighbor))
         return None, []
+
+    def register_ship(self, crew_required):
+        port_unit = self.player.port.tile.unit
+        if port_unit and port_unit.ship_health > 0:
+            # if ship on port
+            crew_needed = crew_required - port_unit.crew
+            if crew_needed > 0:
+                self.ship_queue[port_unit] = crew_needed
+                # returns the ship unit if built all crew needed, False if not
+                return self.build()
+        else:
+            # create ship
+            self.player.port.spawn(SHIP)
 
     # <<-- /Creer-Merge: functions -->>
